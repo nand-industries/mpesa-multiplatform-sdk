d086145462ed1a7f89842aa70e04754b88a40981
Calebe Miquissene <callebmiquissene@gmail.com>
2025-10-12 01:08:17 +0200

    Test beta release

--- buildSrc/src/main/kotlin/ProjectConfigurations.kt
+++ buildSrc/src/main/kotlin/ProjectConfigurations.kt
@@ -1,6 +1,6 @@
 object ProjectConfigurations {
     object MpesaMultiplatformSdk {
-        const val versionName = "1.0.0"
+        const val versionName = "1.0.1-beta1"
         const val packageOfResClass = "io.github.nandindustries.sdk.resources"
         const val namespace = "io.github.nandindustries.android"
         const val baseName = "MpesaMultiplatformSdk"
